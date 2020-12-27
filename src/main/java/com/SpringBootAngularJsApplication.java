92de86e65155ceaec327b63b86617d96e6a720db
krasnovss91 <krasnovstanislav91@gmail.com>
2020-12-27 21:59:27 +0400

    first commit

---
+++ src/main/java/com/SpringBootAngularJsApplication.java
@@ -0,0 +1,11 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAngularJsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAngularJsApplication.class, args);
+    }
+}
